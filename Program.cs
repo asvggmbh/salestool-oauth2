8a886f28d37bc0e9fde54bcaa6cbf43868342832
Christian Schmitt <c.schmitt@envisia.de>
2023-11-29 10:12:44 +0100

    initial project

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DemoSalestoolLogin.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication().AddOpenIdConnect("asvg", options =>
+{
+    options.Authority = "https://salestool.asvg-solutions.de/";
+    options.MetadataAddress = "https://salestool.asvg-solutions.de/.well-known/openid-configuration";
+    options.ClientId = "Weyrauch";
+    options.Scope.Add("profile");
+    options.Scope.Add("openid");
+
+    options.NonceCookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+
+    options.GetClaimsFromUserInfoEndpoint = false;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+
+    options.Events = new OpenIdConnectEvents
+    {
+        OnRemoteFailure = ctx =>
+        {
+            if (ctx.Failure?.Message == "Correlation failed.")
+            {
+                ctx.Response.Redirect("/");
+                ctx.HandleResponse();
+            }
+
+            return Task.CompletedTask;
+        },
+    };
+});
+
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
